790ad47f117d00d17e56f5a18b93b5f575e4d36b
dhirendrathakulla <dhiren.thakulla@gmail.com>
2023-10-30 18:35:01 -0400

    netlify deploy serverless

---
+++ functions/api.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const serverless = require("serverless-http");
+const app = express();
+
+const router = express.Router()
+
+router.get ("/",(req,res ) => {
+    res.json({
+        "hello":"hi"
+    })
+})
+router.get ("/t",(req,res ) => {
+    res.json({
+        "hello":"test"
+    })
+})
+app.use("/.netlify/functions/api",router)
+module.exports.handler = serverless(app)
